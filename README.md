8cbac18a7661c9fd7ed881ff79242edb9c752c69
Juan Senen <juansenen@hotmail.com>
2022-10-23 21:33:00 +0200

    Finish U2. Change README.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PHP_lecciones
+Lineas de código que recogen diferentes aspectos del lenguaje php.
+
+El directorio se compone de varias unidades en las que se explican diferentes aspectos.
+
+Puedes abrir el código para verlo en el IDE de tu elección.
+
+Los resultados se pueden ver en el navegador.
--- SA_Basic_Programing_Instrucctions/U2_Control_Structures.php
+++ SA_Basic_Programing_Instrucctions/U2_Control_Structures.php
@@ -49,7 +49,119 @@ $message = "";
 
 <!-- SWITCH -->
 <h3>SWITCH</h3>
-<h3>MATCH</h3>
+<?php
+$day = 'Monday';
+switch ($day) {
+    case 'Monday':
+        $offer = '20% off chocolates';
+        break;
 
+    case 'Tuesday':
+        $offer = '10% off Candys';
+        break;
+    case 'Wednesday':
+        $offer = '10% of cakes';
+        break;
+    default:
+        $offer = '5% of cokies';
+}
+?>
+<h4>The Cnady store offers every week day</h4>
+<p>Offers of the day</p>
+<p>Today is <?= $day ?>, and our offers is <?= $offer ?></p>
+<h3>MATCH</h3>
+<?php
+$day = 'Thursday';
+
+$offer = match($day) {
+    'Monday'    =>'20% offers of chees',
+    'Wednesday' =>'15% offers of chocolates',
+    'Thursday'   => '5% offers of coffe',
+    default     => 'No offer',
+}
+?>
+<p>today is <?= $day ?>, our offer for today it is <?= $offer ?> </p>
+<!-- LOOPS -->
+<h3>WHILE LOOPS</h3>
+<?php
+$counter = 1;
+$pack = 5;
+$price = 1.99;
+?>
+<p>Price for Multiple Packs</p>
+<p>
+    <?php
+    while ($counter <= $pack){
+        echo $counter;
+        echo ' packs cost $';
+        echo $price * $counter;
+        echo '<br>';
+        $counter++;
+    }
+    ?>
+</p>
+<!-- DO WHILE -->
+<h3>DO WHILE</h3>
+<?php
+$pack = 5;
+$price = 1.99;
+?>
+<p>Price for Multiple Packs</p>
+<p>
+    <?php
+    do {
+        echo $pack;
+        echo ' packs cost $';
+        echo $price * $pack;
+        echo '<br>';
+        $pack--;
+    } while($pack > 0);
+
+    ?>
+</p>
+<!-- FOR LOOPS -->
+<h3>FOR LOOPS</h3>
+<?php
+$price = 1.99;
+?>
+<p>Price for large Orders</p>
+<?php
+for($i = 0; $i <=100; $i = $i + 10){
+    echo $i;
+    echo ' pack cost $';
+    echo $price * $i;
+    echo '<br>';
+}
+?>
+<!-- FOREACH-->
+<h3>FOREACH LOOPS</h3>
+<?php
+$products = [
+        'Toffe' => 2.99,
+        'Mints' => 1.99,
+        'Fudge' => 3.49,
+];
+?>
+<p>Price List</p>
+<table>
+    <tr>
+        <th>Item</th>
+        <th>Price</th>
+    </tr>
+    <?php
+    foreach ($products as $item => $price) { ?>
+    <tr>
+        <td><?= $item ?></td>
+        <td><?= $price ?></td>
+    </tr>
+    <?php } ?>
+
+</table>
+
+<!-- INCLUDE FILES-->
+<h3>INCLUDE FILES</h3>
+<?php include 'includes/header.php'; ?>
+<h3>The Candy Store</h3>
+<h3>Welcome</h3>
 </body>
 </html>
