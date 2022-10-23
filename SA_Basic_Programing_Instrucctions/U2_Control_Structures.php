217682273510e6bbedd8ea4436dd603a18736b42
Juan Senen <juansenen@hotmail.com>
2022-10-23 12:04:11 +0200

    Updates

--- SA_Basic_Programing_Instrucctions/U2_Control_Structures.php
+++ SA_Basic_Programing_Instrucctions/U2_Control_Structures.php
@@ -1,4 +1,10 @@
 <?php
+$name = 'Ivy';
+$gretting = 'Welcome';
+$stock = 5;
+$stockb = 15;
+$stockc = 35;
+$message = "";
 ?>
 <<!doctype html>
 <html lang="en">
@@ -10,6 +16,40 @@
     <title>Chapter 2</title>
 </head>
 <body>
+<!-- IF --->
+<h3>IF</h3>
+<p>El stock es <?= $stock ?> . Condicion que sea mayor de 5</p>
+<?php if ($stock > 5){
+    $message = " El Stock es mayor";
+}
+?>
+<p><?= $message ?> </p>
+<!-- IF.. ELSE --->
+<h3>IF .. ELSE</h3>
+<p>El stock es <?= $stock ?> . Condicion que sea mayor de 5</p>
+<?php if ($stock > 5){
+    $message = " El Stock es mayor de 5";
+}else {
+    $message =" El stock es menor";
+}
+?>
+<p><?= $message ?> </p>
+<!-- IF ELSEIF --->
+<h3>IF .. ELSEIF</h3>
+<p>El stock es <?= $stock ?> . Condicion que sea mayor de 5 o mayor de 10</p>
+<?php if ($stock > 5){
+    $message = " El Stock es mayor de 5";
+}elseif ($stock > 10) {
+    $message =" El stock es mayor de 10";
+}elseif ($stock < 5 ){
+    $message =" El stock es menor de 5";
+}
+?>
+<p><?= $message ?> </p>
+
+<!-- SWITCH -->
+<h3>SWITCH</h3>
+<h3>MATCH</h3>
 
 </body>
 </html>
